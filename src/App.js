21b5ff6e018fee16163660f25a7ec8b1242a4e82
bbriggs1979 <bbriggs1979@gmail.com>
2018-10-27 17:44:25 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import Main from './pagedraw/main';
-
+import Main from './main';
 
 class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        text:''
+      };
+    }
   render() {
-    return (
-        <Main/>
-    );
+    return <Main
+    text={this.state.text}
+    onClick={this.onClick.bind(this)}/>
+  }  
+  onClick () {
+    alert('hello');
+
+    this.setState({
+      text:'$19.99'
+    });
   }
 }
-
 export default App;
