346a4da7a8c85a433cef6fb2e9a0b6b4795aac2c
bbriggs1979 <bbriggs1979@gmail.com>
2018-10-27 20:02:40 -0400

    Class Project

---
+++ build/precache-manifest.157847883a147771b16efc291a5e8067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a237d5aa3c5a90e6b73",
+    "url": "/static/js/main.2a237d5a.chunk.js"
+  },
+  {
+    "revision": "a3acffde1786d9710ca7",
+    "url": "/static/js/1.a3acffde.chunk.js"
+  },
+  {
+    "revision": "2a237d5aa3c5a90e6b73",
+    "url": "/static/css/main.40cc9ce2.chunk.css"
+  },
+  {
+    "revision": "cfad0c99bcdf2e234546123b6fd5afc7",
+    "url": "/index.html"
+  }
+];
